382d5bbd9f54ee56447e126b04013a68ccdea422
Shabareesh S Gowda <shabareesh390@gmail.com>
2026-06-19 20:54:09 +0530

    Remove Vercel build artifacts

--- .vercel/output/functions/__server.func/_tanstack-start-manifest_v-UAp4vjl-.mjs
+++
@@ -1,4 +0,0 @@
-const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/Shabs_portfolio/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-CY5dtm2W.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-CY5dtm2W.js" } }] }, "/": { filePath: "D:/Shabs_portfolio/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-C1n11zJf.js"] } } });
-export {
-  tsrStartManifest
-};
